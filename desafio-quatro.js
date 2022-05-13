15729128804fd184617fa69d93165e3a7834b2ae
Vivian Azevedo <viviancrisga@gmail.com>
2022-05-13 15:50:44 +1000

    Loop continua não funcionando mas evoluiu

--- desafio-quatro.js
+++ desafio-quatro.js
@@ -2,15 +2,17 @@ const prompt = require("prompt-sync")();
 
 const numeroSorteado = 6;
 
-let numero = prompt("Digite um número entre 1 a 10.  ");
-
 let mensagem = "";
 
 for (let i = 0; i < 3; i++) {
+  numero = prompt("Digite um número entre 1 a 10.  ");
   if (numero == numeroSorteado) {
     mensagem += ` ${numero} Parabéns, você acertou! `;
+    console.log(mensagem);
   } else {
     mensagem += ` ${numero} Que pena, você errou! `;
+    console.log(mensagem);
   }
 }
-console.log(mensagem);
+
+//console.log(mensagem);

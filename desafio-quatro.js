c7028c880e305e1cd74688406a8b35b2aec5f1cd
Vivian Azevedo <viviancrisga@gmail.com>
2022-05-13 15:43:58 +1000

    Loop funciona mas não da maneira correta

--- desafio-quatro.js
+++ desafio-quatro.js
@@ -4,14 +4,13 @@ const numeroSorteado = 6;
 
 let numero = prompt("Digite um número entre 1 a 10.  ");
 
-console.log(`O número que você digitou foi ${numero}`);
-
 let mensagem = "";
 
-if (numero == numeroSorteado) {
-  mensagem = `${numero} Parabéns, você acertou!`;
-} else {
-  mensagem = `${numero} Que pena, você errou!`;
+for (let i = 0; i < 3; i++) {
+  if (numero == numeroSorteado) {
+    mensagem += ` ${numero} Parabéns, você acertou! `;
+  } else {
+    mensagem += ` ${numero} Que pena, você errou! `;
+  }
 }
-
 console.log(mensagem);

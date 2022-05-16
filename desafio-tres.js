1fd36ac7b1e41d251401f7c82767781ce0874464
Vivian Azevedo <viviancrisga@gmail.com>
2022-05-16 18:17:04 +1000

    ITem 3 foi adicionado mas nào funciona direito

--- desafio-tres.js
+++ desafio-tres.js
@@ -15,4 +15,30 @@ if (questao == opcaoUm) {
   mensagem = prompt(`Você gostaria de estudar C# ou Java?   `);
 }
 mensagem = `Que legal que você deseja se desenvolver em ${questao}! `;
+
 console.log(mensagem);
+
+const opUm = "Especializar";
+const opDois = "Fullstack";
+
+let questaoDois = prompt(
+  `Você deseja entre seguir se especializando na área escolhida
+  ou seguir se desenvolvendo para se tornar Fullstack? 
+ Responda se ${opUm} ou se tornar ${opDois}   `
+);
+
+let mensagemDois = "";
+
+if (questaoDois == opUm) {
+  mensagemDois = prompt(
+    `É muito importante procurar entender e desenvolver novos conhecimentos a fim de aprimorar suas atividades.`
+  );
+} else if (questaoDois == opDois) {
+  mensagemDois = prompt(`Continue se esforçando para se tornar habilitado
+  para compreender e operar em todas as camadas do desenvolvimento de um projeto!`);
+} else {
+}
+
+let mensagemFinal = `Que legal você deseja se ${questaoDois}! `;
+
+console.log(mensagemDois);

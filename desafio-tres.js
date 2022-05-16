84811e328efbb062fb8bf57a923d6b4d63c29a20
Vivian Azevedo <viviancrisga@gmail.com>
2022-05-16 17:52:43 +1000

    Primeiro e segundo item adicionados

--- desafio-tres.js
+++ desafio-tres.js
@@ -1,13 +1,18 @@
 const prompt = require("prompt-sync")();
 
-const questao = prompt(
-  `Qual é a área que você deseja desenvolver? Responda: Front End ou Back End    `
+const opcaoUm = "Front End";
+const opcaoDois = "Back End";
+
+let questao = prompt(
+  `Qual é a área que você deseja desenvolver? Responda: ${opcaoUm} ou ${opcaoDois}    `
 );
 
-const resposta = `Que legal sua escolha por ${questao}  `;
-
-console.log(resposta);
-
-let area = "";
-
 let mensagem = "";
+
+if (questao == opcaoUm) {
+  mensagem = prompt(`Você gostaria de estudar React ou Vue?  `);
+} else if (questao == opcaoDois) {
+  mensagem = prompt(`Você gostaria de estudar C# ou Java?   `);
+}
+mensagem = `Que legal que você deseja se desenvolver em ${questao}! `;
+console.log(mensagem);

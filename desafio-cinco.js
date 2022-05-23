8463d53e91240d714f82013aaa99976408594362
Vivian Azevedo <viviancrisga@gmail.com>
2022-05-23 17:06:30 +1000

    Testando inserir o primeiro item da lista de compras

--- desafio-cinco.js
+++ desafio-cinco.js
@@ -1,3 +1,9 @@
 const prompt = require("prompt-sync")();
 
 const mensagem = prompt(`Deseja adicionar um item na sua lista de compras? `);
+
+let itens = [];
+
+let listaDeCompras = itens.push("Limão");
+
+console.log(itens);

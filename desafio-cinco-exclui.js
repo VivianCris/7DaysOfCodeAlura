a0e011af31f5fbcff63195a23e5c6ce10655d959
Vivian Azevedo <viviancrisga@gmail.com>
2022-05-27 16:25:10 +1000

    Iniciando a terceira parte do desafio 5

---
+++ desafio-cinco-exclui.js
@@ -0,0 +1,46 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+const respostaUm = "S";
+const respostaDois = "N";
+
+let itens = [];
+let vegetais = [];
+let higiene = [];
+
+while (true) {
+  const perguntaUm = "Deseja adicionar um item na sua lista de compras?";
+
+  const resposta = prompt(
+    ` ${perguntaUm} Responda ${respostaUm} ou ${respostaDois}  `
+  );
+
+  if (resposta === respostaDois) {
+    console.log(`Nenhum item será adicionado a lista!`);
+    break;
+  }
+
+  let produto = prompt(`Qual?   `);
+
+  itens.push(produto);
+
+  const perguntaDois = "Qual categoria esse item se encaixa: ";
+
+  const categoria = prompt(`${perguntaDois} "vegetais", "higiene"?   `);
+
+  if (categoria === "vegetais") {
+    vegetais.push(produto);
+  } else if (categoria === "higiene") {
+    higiene.push(produto);
+  }
+
+  console.log(itens);
+}
+
+console.log("Vegetais: " + vegetais + ", Higiene: " + higiene);
+
+const listaDeCompras = [vegetais, higiene];
+
+const perguntaTres = "Deseja excluir algum item da lista de compras?";
+
+const perguntaTresA = prompt(`${perguntaTres} Responda 1 para Sim   `);
+const excluirItem = prompt(` Qual?   `);

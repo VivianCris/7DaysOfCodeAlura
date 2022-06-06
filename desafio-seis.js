19898400f9bb622ac1351ebad698fb359fa65418
Vivian Azevedo <viviancrisga@gmail.com>
2022-06-06 14:37:33 +1000

    Adicionando as funcoes de cada operacao matemática

--- desafio-seis.js
+++ desafio-seis.js
@@ -1,4 +1,8 @@
 const prompt = require("prompt-sync")({ sigint: true });
+
+//---------------------- Início do programa -----------------------
+// Usuário escolhe a operação
+
 const escolherOperacao = "Digite a operação que você deseja: ";
 
 const perguntaOperacao = prompt(
@@ -6,3 +10,29 @@ const perguntaOperacao = prompt(
 );
 
 console.log(perguntaOperacao);
+
+// --------------------------- Funções para cada operação --------------
+
+function somar(primeiroNumero, segundoNumero) {
+  return primeiroNumero + segundoNumero;
+}
+
+console.log(somar(12, 14));
+
+function subtrair(primeiroNumero, segundoNumero) {
+  return primeiroNumero - segundoNumero;
+}
+
+console.log(subtrair(18, 14));
+
+function multiplicar(primeiroNumero, segundoNumero) {
+  return primeiroNumero * segundoNumero;
+}
+
+console.log(multiplicar(12, 14));
+
+function dividir(primeiroNumero, segundoNumero) {
+  return primeiroNumero / segundoNumero;
+}
+
+console.log(dividir(28, 14));

6d718caedfb686bb59ecb21027c1d26bf90e62ad
Vivian Azevedo <viviancrisga@gmail.com>
2022-06-07 21:17:13 +1000

    Adiciocando a funcão de adicão funcionando

--- desafio-seis.js
+++ desafio-seis.js
@@ -5,13 +5,12 @@ const prompt = require("prompt-sync")({ sigint: true });
 
 const sim = "S";
 const nao = "N";
-
 let numerosOperacao = [];
 
-const escolherOperacao = "Digite a operação que você deseja realizar: ";
-const perguntaOperacao = prompt(
-  `${escolherOperacao}1- soma 2- subtrai 3- multiplica 4- divide:  `
-);
+// const escolherOperacao = "Digite a operação que você deseja realizar: ";
+// const perguntaOperacao = prompt(
+//   `${escolherOperacao}1- soma 2- subtrai 3- multiplica 4- divide:  `
+// );
 
 while (true) {
   const perguntaUm = "Deseja adicionar um número para a operação?";
@@ -22,45 +21,20 @@ while (true) {
     break;
   }
 
-  let valoresOperacao = prompt(`Qual?   `);
+  const valoresOperacao = prompt(`Qual?   `);
 
   numerosOperacao.push(valoresOperacao);
+
   console.log(numerosOperacao);
 }
+
 console.log(numerosOperacao);
 // --------------------------- Funções para cada operação --------------
 
-function somar(primeiroNumero, segundoNumero) {
-  return primeiroNumero + segundoNumero;
+function somar(num1, num2) {
+  return Number(num1) + Number(num2);
 }
-
-//console.log(somar(12, 14));
-
-function subtrair(primeiroNumero, segundoNumero) {
-  return primeiroNumero - segundoNumero;
-}
-
-//console.log(subtrair(18, 14));
-
-function multiplicar(primeiroNumero, segundoNumero) {
-  return primeiroNumero * segundoNumero;
-}
-
-//console.log(multiplicar(12, 14));
-
-function dividir(primeiroNumero, segundoNumero) {
-  return primeiroNumero / segundoNumero;
-}
-
-//console.log(dividir(28, 14));
 
 // ---------------- a partir da escolha da operação faz algo -------
 
-// if (perguntaOperacao === "1") {
-// }
-// if (perguntaOperacao === "2") {
-// }
-// if (perguntaOperacao === "3") {
-// }
-// if (perguntaOperacao === "4") {
-// }
+console.log(somar(numerosOperacao[0], numerosOperacao[1]));
